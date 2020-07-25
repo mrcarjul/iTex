5ecd9f66cb637fbdb2fcf4da61120ed007b3b21d
mrcarjul <mrcarjul@gmail.com>
2020-07-25 14:12:19 -0500

    moved content to screen and added redux provider

--- App.js
+++ App.js
@@ -1,86 +1,19 @@
 import React, {useState} from 'react';
 
-// Core
-import {Text, View, StyleSheet} from 'react-native';
-import {
-  Body,
-  Button,
-  Container,
-  Content,
-  Header,
-  Input,
-  Left,
-  Right,
-  Title,
-} from 'native-base';
+import Home from './src/screens/Home';
 
-// Themes
-import {colors} from './src/themes';
+// Redux
+import {Provider} from 'react-redux';
+import configureStore from './src/redux/index';
+
+export const store = configureStore();
 
 function App() {
-  const [employeeId, setEmployeeId] = useState('');
-
-  const onChangeText = (text) => {
-    setEmployeeId(text);
-  };
-
   return (
-    <Container>
-      <Header>
-        <Left />
-        <Body>
-          <Title>iTex</Title>
-        </Body>
-        <Right />
-      </Header>
-      <Content padder>
-        <View style={styles.topContainer}>
-          <Text style={styles.topText} success>
-            Enter your employee code:
-          </Text>
-          <Input value={employeeId} style={styles.input} {...{onChangeText}} />
-          <View style={styles.centerContents}>
-            {employeeId != '' ? (
-              <Text style={styles.greetText}>Hello!, {employeeId}</Text>
-            ) : null}
-          </View>
-          <Button onPress={() => {}} style={styles.button} block>
-            <Text style={styles.buttonText} small primary>
-              Check Data
-            </Text>
-          </Button>
-        </View>
-      </Content>
-    </Container>
+    <Provider store={store}>
+      <Home />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  button: {
-    marginHorizontal: 10,
-    marginVertical: 10,
-  },
-  buttonText: {
-    color: colors.white,
-  },
-  centerContents: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  greetText: {
-    color: colors.lightGray,
-  },
-  input: {
-    borderColor: 'gray',
-    borderWidth: StyleSheet.hairlineWidth,
-  },
-  topContainer: {
-    flex: 1,
-  },
-  topText: {
-    fontSize: 16,
-    color: colors.gray,
-  },
-});
 
 export default App;
