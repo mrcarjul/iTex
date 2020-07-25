18c7aeaaf320f8e50cc10f6c60a59a3f7d00c028
mrcarjul <mrcarjul@gmail.com>
2020-07-25 15:18:16 -0500

    added item component

---
+++ src/components/Item.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+// Core
+import {Text, View} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+// Personalized components
+import ItemText from './ItemText';
+
+// Themes
+import {genericStyles} from '../themes';
+
+/**
+ * @description Item to be rendered at the list
+ * @returns list item if shouldDisplay prop comes with a thruty value 
+ */
+function Item({
+  id,
+  date,
+  type,
+  status,
+  shouldDisplayStatus,
+  shouldDisplay,
+  registryInternalKey,
+}) {
+  if (!shouldDisplay) return null;
+  return (
+    <View key={registryInternalKey} style={styles.item}>
+      <View style={styles.centerContents}>
+        <Text style={[styles.itemIdText, genericStyles.boldText]}>{id}</Text>
+      </View>
+      <View style={styles.itemCenterContainer}>
+        <ItemText title="Date" content={date} />
+        {shouldDisplayStatus && <ItemText title="Status" content={status} />}
+      </View>
+      <View style={styles.centerContents}>
+        <AntDesign name={type === 'clock in' ? 'login' : 'logout'} size={25} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  centerContents: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  item: {
+    flexDirection: 'row',
+    borderColor: colors.blue,
+    borderRadius: 8,
+    borderWidth: StyleSheet.hairlineWidth * 2,
+    minHeight: 60,
+    padding: 10,
+    marginVertical: 10,
+  },
+  itemCenterContainer: {
+    flex: 4,
+  },
+  itemIdText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default Item;
