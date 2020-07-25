8663c2a5a61b55bb300d623f871b50a8bc7687f8
mrcarjul <mrcarjul@gmail.com>
2020-07-25 13:45:26 -0500

    created initial version for our api calls

---
+++ src/services/EmployeesApi.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const API_URL = 'https://us-central1-app-tiendita.cloudfunctions.net';
+
+/**
+ * Manages the axios  api call instance creation and returns the available endpoints
+ * @param {string} baseURL Url to be used for api calls
+ */
+const create = (baseURL = API_URL) => {
+  const api = axios.create({
+    baseURL,
+    timeout: 25000,
+  });
+
+  /**
+   * @description Sends api call to retrive employee data by given id
+   * @param {number} employeeId
+   */
+  const getEmployeeData = (employeeId) =>
+    api.post(`/simpleExerciseTest?id=${employeeId}`);
+
+  return {
+    getEmployeeData,
+  };
+};
+
+const API = create();
+
+export default API;
